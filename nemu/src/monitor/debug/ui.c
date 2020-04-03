ab3439ab07e7634c158ae9fc0e67af73f251670f
tracer-ics2017 <tracer@njuics.org>
2020-04-03 15:15:43 +0800

    >  compile 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  15:15:43 up 12:14,  1 user,  load average: 0.01, 0.02, 0.00 4a519710301d27cbda3b4754c62ef3abb8dea42

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,14 +37,14 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_si(char *args){ 
-	printf(".....\n");
+	//printf(".....\n");
 	char *token = strtok(args," ");
 	//token = strtok(NULL," ");
 	int num = atoi(token);
-	printf("%d \n",num);	
+	//printf("%d \n",num);	
 	cpu_exec(num);
-	printf("%d \n",num);
-	printf("this is cmd si.\n");
+	//printf("%d \n",num);
+	//printf("this is cmd si.\n");
 	return 0;
 }
 
