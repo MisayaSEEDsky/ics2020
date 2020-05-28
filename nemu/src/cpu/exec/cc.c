5d525ea3bf69bf98f1fe383dda27f88f44cc9af5
tracer-ics2017 <tracer@njuics.org>
2020-05-28 15:41:07 +0800

    >  compile 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  15:41:07 up 3 days, 18:33,  3 users,  load average: 0.07, 0.03, 0.01 5cbda77adce7ef66fc522d49b7f4711a6d6b9bf

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -5,23 +5,50 @@
 void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   bool invert = subcode & 0x1;
   enum {
-    CC_O, CC_NO, CC_B,  CC_NB,
-    CC_E, CC_NE, CC_BE, CC_NBE,
-    CC_S, CC_NS, CC_P,  CC_NP,
-    CC_L, CC_NL, CC_LE, CC_NLE
+    CC_O, CC_NO, CC_B,  CC_NB,	//0 ~ 3
+    CC_E, CC_NE, CC_BE, CC_NBE,	//4 ~ 7
+    CC_S, CC_NS, CC_P,  CC_NP,	//8 ~ 11
+    CC_L, CC_NL, CC_LE, CC_NLE	//12 ~ 15
   };
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
+	rtl_get_OF(dest);
+	break;
     case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+	rtl_get_CF(dest);
+	break;
+    case CC_E://4
+	rtl_get_ZF(dest);
+	break;
+    case CC_BE://6
+	{
+		rtl_get_CF(&t0);
+		rtl_get_ZF(&t1);
+		rtl_or(dest, &t0, &t1);
+	}
+	break;			    
+    case CC_S:	//8
+	rtl_get_SF(dest);
+	break;
+    case CC_L:	//12
+	{
+		rtl_get_SF(&t0);
+		rtl_get_OF(&t1);
+		rtl_xor(dest, &t1, &t0);
+	}
+	break;
+    case CC_LE:	//14
+	{
+		rtl_get_ZF(&t0);
+		rtl_get_SF(&t1);
+		rtl_get_OF(&t2);
+		rtl_xor(&t3,&t1,&t2);
+		rtl_or(dest, &t0 , &t3);
+	}
+	break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
