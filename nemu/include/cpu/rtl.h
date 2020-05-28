ef202381bd7885c14c9137b3c112908a8a8cf623
tracer-ics2017 <tracer@njuics.org>
2020-05-28 21:30:56 +0800

    >  compile 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  21:30:56 up 4 days, 23 min,  3 users,  load average: 0.00, 0.02, 0.00 bd3b3bdf9d1a640f9b9e009fb5232afd59d1b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -3,7 +3,7 @@
 
 #include "nemu.h"
 
-extern rtlreg_t t0, t1, t2, t3;
+extern rtlreg_t t0, t1, t2, t3,t4,t5;
 extern const rtlreg_t tzero;
 
 /* RTL basic instructions */
@@ -194,18 +194,18 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  t3 = *result;
+  t4 = *result;
   switch(width)
   {
 	  case 1:
-		  t3 = (*result * 0x000000ff);
+		  t4 = (*result * 0x000000ff);
 		  break;
 	  case 2:
-		  t3 = (*result & 0x0000ffff);
+		  t4 = (*result & 0x0000ffff);
 		  break;
   }
-  rtl_eq0(&t1, &t3);
-  rtl_set_ZF(&t1);
+  rtl_eq0(&t5, &t4);
+  rtl_set_ZF(&t5);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
