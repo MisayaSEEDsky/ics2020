a2b508b9ae8b1740af25337c3e8cfa8cde4f2160
tracer-ics2017 <tracer@njuics.org>
2020-05-28 21:05:10 +0800

    >  compile 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  21:05:10 up 3 days, 23:57,  3 users,  load average: 0.08, 0.03, 0.00 19bb6376f547ef1ffa6f3900e5fda4a8531445d

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -35,8 +35,8 @@ make_EHelper(xor) {
   }
   else assert(0);
   t1 = 0;
-  rtl_set_OF(&t3);
-  rtl_set_CF(&t3);
+  rtl_set_OF(&t2);
+  rtl_set_CF(&t2);
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
 
 
