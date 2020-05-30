4135cfc2f6a2121b6bcdc38da58f3355c489a4bc
tracer-ics2017 <tracer@njuics.org>
2020-05-30 11:50:59 +0800

    >  compile 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  11:50:59 up  1:58,  4 users,  load average: 0.00, 0.06, 0.18 dc8e183a8aa0d3ffc2524cccfc2585ea9482fd

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -108,11 +108,12 @@ make_EHelper(rol) {
   for(t0 = 0;t0 < id_src->val ; t0++)
   {
 	  rtl_shri(&t1, &id_dest->val, id_dest->width * 8 - 1);
-	  rtl_shli(&t1, &id_dest->val, 1);
-	  rtl_xori(&t2,&id_dest->val,t1);
+	  rtl_shli(&t2, &id_dest->val, 1);
+	  //rtl_xori(&t2,&id_dest->val,t1);
+	  id_dest->val = t1 +t2; 
   }
   rtl_set_CF(&t1);
-  operand_write(id_dest,&t2);
+  operand_write(id_dest,&id_dest->val);
   
   print_asm_template2(rol);
 }
