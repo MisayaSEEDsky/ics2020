93b4d85c50cacae040a912ce5047586c6ce0381d
tracer-ics2017 <tracer@njuics.org>
2020-06-11 20:05:05 +0800

    >  gdb 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  20:05:05 up 7 min,  2 users,  load average: 0.05, 0.09, 0.05 96a69ebb46a9faf3bdb957c55effeb7e94a7662d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,6 +3,6 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
