6f9ca8ddfe4641e8ef88b76c7396b6c35f6887d4
tracer-ics2017 <tracer@njuics.org>
2020-06-12 17:55:57 +0800

    >  compile 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  17:55:57 up  2:53,  3 users,  load average: 0.13, 0.03, 0.01 5058d8e6cbe9ecfeb63e66c094c68fde25dce622

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,15 +14,12 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
+typedef union {
    union{
-	   union{
-		   union{
-      			  uint32_t _32;
+    			  uint32_t _32;
       			  uint16_t _16;
-       			   uint8_t _8[2];
-		   };uint32_t val;
-      } gpr[8];
+       			   uint8_t _8[2];  
+   } gpr[8];
     struct{
     /* Do NOT change the order of the GPRs' definitions. */
 
@@ -31,8 +28,7 @@ typedef struct {
     */
       rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
       vaddr_t eip;
-    };
-   };
+    
     union {
 	struct {
 		uint32_t CF:1;
@@ -53,7 +49,7 @@ typedef struct {
 	    uint32_t base;	// 32 base
 	    uint16_t limit;	// 16 limit
     }idtr;
-
+   };
 } CPU_state;
 
 extern CPU_state cpu;
