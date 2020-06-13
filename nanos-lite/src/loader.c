beb5025bc7b198e5ac6628dc058d8aafa1e9e8ae
tracer-ics2017 <tracer@njuics.org>
2020-06-13 12:37:38 +0800

    >  gdb 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  12:37:38 up  1:14,  3 users,  load average: 0.06, 0.03, 0.00 4826a43edd70872c2ca600e65f10925a813b66b

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,11 +1,16 @@
 #include "common.h"
-
+#include "fs.h"
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
-  return (uintptr_t)DEFAULT_ENTRY;
+  //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
+  	int fd = fs_open(filename, 0 ,0);
+	size_t lens = fs_filesz(fd);
+	fs_read(fd,DEFAULT_ENTRY,lens);
+	fs_close(fd);
+
+	return (uintptr_t)DEFAULT_ENTRY;
 }
