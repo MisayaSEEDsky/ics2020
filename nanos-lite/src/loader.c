151d6cdd434ca2a6789d5a8e8f7ee93f424d2ba1
tracer-ics2017 <tracer@njuics.org>
2020-06-22 19:25:13 +0800

    >  gdb 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  19:25:13 up 11:39,  3 users,  load average: 0.04, 0.04, 0.00 15aea3eab3f6c5fe492c081d2c18f6ecf00203ca

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,7 +1,9 @@
 #include "common.h"
 #include "fs.h"
-#define DEFAULT_ENTRY ((void *)0x4000000)
+//#include "mm.c"
+#define DEFAULT_ENTRY ((void *)0x8048000)
 
+void* new_page(void);
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
@@ -9,7 +11,21 @@ uintptr_t loader(_Protect *as, const char *filename) {
   //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   	int fd = fs_open(filename, 0 ,0);
 	size_t lens = fs_filesz(fd);
-	fs_read(fd,DEFAULT_ENTRY,lens);
+	//fs_read(fd,DEFAULT_ENTRY,lens);
+	void *va = DEFAULT_ENTRY;
+	void *pa;
+	int page_count = lens  /  PGSIZE + 1;
+
+	for(int i = 0; i < page_count; i++)
+	{
+		pa = new_page();
+		Log("Map va to pa: 0x%08x to 0x%08x",va,pa);
+		_map(as,va,pa);
+		fs_read(fd,pa,PGSIZE);
+		va += PGSIZE;
+	}
+	
+	
 	fs_close(fd);
 
 	return (uintptr_t)DEFAULT_ENTRY;
