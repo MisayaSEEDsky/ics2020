9d96e71b31511ef4d55499222550692bc7848556
tracer-ics2017 <tracer@njuics.org>
2020-06-22 19:35:22 +0800

    >  gdb 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  19:35:22 up 11:49,  3 users,  load average: 0.17, 0.04, 0.01 1524e29f64c7d317bdda0cfebde554a677900391

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -34,8 +34,8 @@ static inline uintptr_t sys_close(_RegSet *r, uintptr_t fd) {
 
 static inline uintptr_t sys_brk(_RegSet *r) {
   //TODO();
-  	SYSCALL_ARG1(r) = 0;
-	return 1;
+  	SYSCALL_ARG1(r) = mm_brk(SYSCALL_ARG2(r));
+	return 0;
 }
 
 static inline uintptr_t sys_none(_RegSet *r)
