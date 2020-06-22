890a743d6190ac1748a56eec36fa85eee0abf0ff
tracer-ics2017 <tracer@njuics.org>
2020-06-22 23:49:31 +0800

    >  compile 161820317 zhangshuai Linux zhangshuai 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  23:49:30 up 16:00,  3 users,  load average: 0.03, 0.01, 0.00 7e4633e184f14898dcb4dfcd3d9fb5c988d67ad7

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -67,14 +67,14 @@ paddr_t page_translate(vaddr_t addr, bool is_write) {
   if (!pde.present) {
     //Log("page_translate: addr: 0x%x\n", addr);
     //Log("page_translate: dir: 0x%x page: 0x%x offset: 0x%x PDT_base: 0x%x\n", dir, page, offset, PDT_base);
-    //assert(pde.present);
+    assert(pde.present);
   }
   PTE pte;
   // Log("page_translate: page_frame: 0x%x\n", pde.page_frame);
   pte.val = paddr_read((pde.page_frame << 12) + (page << 2), 4);
   if (!pte.present) {
     //Log("page_translate: addr: 0x%x\n", addr);
-    //assert(pte.present);
+    assert(pte.present);
   }
   paddr_t paddr = (pte.page_frame << 12) | offset;
   //Log("page_translate: paddr: 0x%x\n", paddr);
